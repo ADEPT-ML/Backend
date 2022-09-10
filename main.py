a96b6eb1242e68523819a2fe53e37e92f186e1db
Benedikt Tobias <61744142+benedikttobias@users.noreply.github.com>
2022-09-10 15:02:28 +0200

    Created backend api schema

--- main.py
+++ main.py
@@ -16,3 +16,43 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"Hello": "there!"}
+
+
+@app.get("/buildings")
+def read_buildings():
+    pass
+
+
+@app.get("/building/{building}/sensors")
+def read_building_sensors(building: str):
+    pass
+
+
+@app.get("/building/{building}/sensor/{sensor}")
+def read_building_sensor(building: str, sensor: str):
+    pass
+
+
+@app.get("/building/{building}/timestamps")
+def read_building_timestamps(building: str):
+    pass
+
+
+@app.get("/algorithms")
+def read_algorithms():
+    pass
+
+
+@app.get("/calculate/anomalies")
+def read_anomalies(algo: int, building: str, sensors: str, start: str, stop: str):
+    pass
+
+
+@app.get("/calculate/prototypes")
+def read_prototypes(anomaly: int):
+    pass
+
+
+@app.get("/calculate/feature-attribution")
+def read_feature_attribution(anomaly: int):
+    pass
