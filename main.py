af6f516c105d9db63db6d3860fc6f51670e7b2f9
Benedikt Tobias <61744142+benedikttobias@users.noreply.github.com>
2022-11-27 17:37:34 +0100

    Implemented a standardised api root in all services

--- main.py
+++ main.py
@@ -15,9 +15,38 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "there!"}
+@app.get(
+    "/",
+    name="Root path",
+    summary="Returns the routes available through the API",
+    description="Returns a route list for easier use of API through HATEOAS",
+    response_description="List of urls to all available routes",
+    responses={
+        200: {
+            "content": {
+                "application/json": {
+                    "example": {
+                        "payload": [
+                            {
+                                "path": "/examplePath",
+                                "name": "example route"
+                            }
+                        ]
+                    }
+                }
+            },
+        }
+    }
+)
+async def root():
+    """Root API endpoint that lists all available API endpoints.
+
+    Returns:
+        A complete list of all possible API endpoints.
+    """
+    route_filter = ["openapi", "swagger_ui_html", "swagger_ui_redirect", "redoc_html"]
+    url_list = [{"path": route.path, "name": route.name} for route in app.routes if route.name not in route_filter]
+    return url_list
 
 
 @app.get("/buildings")
